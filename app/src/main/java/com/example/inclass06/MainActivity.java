ed5dc9790a83bec531411028935b52e3dec9e56d
Joshua Hutcheson <jhutch50@uncc.edu>
2019-09-30 19:31:05 -0400

    Added isConnected and initial Async

--- app/src/main/java/com/example/inclass06/MainActivity.java
+++ app/src/main/java/com/example/inclass06/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button button;
 
-
+ // comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
