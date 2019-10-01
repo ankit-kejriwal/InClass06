a10f8dd5adacb5e67f1cd6e4e60ae7b788f39d29
ankit <ankitkejriwaal@gmail.com>
2019-09-30 20:24:20 -0400

    added data parser

--- app/src/main/java/com/example/inclass06/MainActivity.java
+++ app/src/main/java/com/example/inclass06/MainActivity.java
@@ -9,11 +9,16 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -29,6 +34,17 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    TextView textViewCategory;
+    ImageView imageViewnext;
+    ImageView imageViewprevious;
+    TextView textViewTitle;
+    TextView textViewDate;
+    ImageView imageViewURL;
+    EditText editTextDesc;
+    TextView textViewcount;
+
+    ArrayList<Article> articleList = new ArrayList<>();
+    int currentIndex = 0;
 
  // comment new da ds
     @Override
@@ -36,16 +52,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
+        textViewCategory = findViewById(R.id.textViewCategory);
+        textViewTitle = findViewById(R.id.textViewTitle);
+        textViewDate = findViewById(R.id.textViewDate);
+        imageViewURL = findViewById(R.id.imageViewURL);
+        editTextDesc = findViewById(R.id.editTextDesc);
+        imageViewnext = findViewById(R.id.imageViewNEXT);
+        imageViewprevious = findViewById(R.id.imageViewPrev);
+        textViewcount = findViewById(R.id.textViewCount);
+        editTextDesc.setEnabled(false);
 
        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] category = {"entertainment","general","health","science","sports","technology"};
+                final String[] category = {"entertainment","general","health","science","sports","technology"};
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("Choose Category");
                 builder.setItems(category, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        String url = "https://newsapi.org/v2/top-headlines?category="+category[which]+"&apiKey=5c72617fea584de7bb9521936989e3c5";
+                        new GetSimpleAsync().execute(url);
                     }
                 });
                 AlertDialog dialog = builder.create();
@@ -66,67 +93,74 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-//    private class GetSimpleAsync extends AsyncTask<String, Void, ArrayList<Article>> {
-//        @Override
-//        protected ArrayList<Article> doInBackground(String... params) {
-//            StringBuilder stringBuilder = new StringBuilder();
-//            HttpURLConnection connection = null;
-//            BufferedReader reader = null;
-//            ArrayList<Article> result = new ArrayList<Article>();
-//            try {
-//                URL url = new URL(params[0]);
-//                connection = (HttpURLConnection) url.openConnection();
-//                connection.connect();
-//                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-//                    reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//                    String line = "";
-//                    while ((line = reader.readLine()) != null) {
-//                        stringBuilder.append(line);
-//                    }
-////                    String json = stringBuilder.toString();
-////                    JSONObject root = new JSONObject(json);
-////                    JSONArray persons = root.getJSONArray("persons");
-////                    for (int i = 0; i < persons.length(); i++) {
-////                        JSONObject personJSON = persons.getJSONObject(i);
-////                        Person person = new Person();
-////                        person.name = personJSON.getString("name");
-////                        person.id = personJSON.getLong(("id"));
-////                        person.age = personJSON.getInt("age");
-////
-////                        JSONObject addressJSON = personJSON.getJSONObject("address");
-////
-////                        Address address = new Address();
-////                        address.line1 = addressJSON.getString("line1");
-////                        address.city = addressJSON.getString("city");
-////                        address.state = addressJSON.getString("state");
-////                        address.zip = addressJSON.getString("zip");
-////                        person.address = address;
-////                        result.add(person);
-////
-////                    }
-//                }
-//            } catch (MalformedURLException e) {
-//                e.printStackTrace();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-////            catch (JSONException e) {
-////                e.printStackTrace();
-////            }
-//            finally {
-//                if (connection != null) {
-//                    connection.disconnect();
-//                }
-//                if (reader != null) {
-//                    try {
-//                        reader.close();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//            return result;
-//        }
-//    }
+    private class GetSimpleAsync extends AsyncTask<String, Void, ArrayList<Article>> {
+        @Override
+        protected ArrayList<Article> doInBackground(String... params) {
+            StringBuilder stringBuilder = new StringBuilder();
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            ArrayList<Article> result = new ArrayList<Article>();
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    String json = stringBuilder.toString();
+//                    Log.d("demo",json);
+                    JSONObject root = new JSONObject(json);
+                    JSONArray articles = root.getJSONArray("articles");
+                    for(int i=0;i<20;i++){
+                        JSONObject articleJSON = articles.getJSONObject(i);
+                        Article article = new Article();
+                        article.title = articleJSON.getString("title");
+                        article.publishedAt = articleJSON.getString("publishedAt");
+                        article.urlToImage = articleJSON.getString("urlToImage");
+                        article.description = articleJSON.getString("description");
+                        result.add(article);
+                    }
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Article> articles) {
+            super.onPostExecute(articles);
+            articleList = articles;
+            setDisplay(currentIndex);
+        }
+    }
+
+    private void setDisplay(int index) {
+        Log.d("demo",articleList.get(index).toString());
+        textViewTitle.setText(articleList.get(index).getTitle());
+        textViewDate.setText(articleList.get(index).getPublishedAt());
+        editTextDesc.setText(articleList.get(index).getDescription());
+        textViewcount.setText(currentIndex+1 +" out of "+ articleList.size());
+    }
+
+
 
 }
